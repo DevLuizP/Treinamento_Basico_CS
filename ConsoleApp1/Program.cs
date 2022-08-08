85fdb73bf06a1232da91c8ae14a17e99375a5eb5
Luiz Paulo Cardoso <70473328+DevLuizP@users.noreply.github.com>
2022-08-08 17:39:47 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿string firstFriend = "Ana";
+string secondFriend = "Frederick Franz";
+
+//Uso dos caracteres '${}' para concatenar as strings com variárias
+Console.WriteLine($"My friends are {firstFriend} and {secondFriend}");
+
+//O Método '.Length' retorna um inteiro após calcular a quantidade de caracteres em uma string
+Console.WriteLine($"The name {firstFriend} has {firstFriend.Length} letters.");
+
+/*O Método '.Replace' substitui caracteres em uma string
+ * No caso abaixo, o utilizo para retirar os espaços em branco ao declarar (" ", "")
+*/
+Console.WriteLine($"The name {secondFriend} has {secondFriend.Replace(" ", "").Length} letters.");
